c2de3bcb585860614e19090a6fac59c4b753d7b1
Ruben <ruben@publicinput.com>
2023-11-28 11:56:19 -0500

    add react js packages and configuration

--- portfolio/Program.cs
+++ portfolio/Program.cs
@@ -1,4 +1,16 @@
+
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using JavaScriptEngineSwitcher.V8;
+using React.AspNet;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddReact();
+
+// Make sure a JS engine is registered, or you will get an error!
+builder.Services.AddJsEngineSwitcher(options => options.DefaultEngineName = V8JsEngine.EngineName)
+    .AddV8();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -14,6 +26,27 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Initialise ReactJS.NET. Must be before static files.
+app.UseReact(config =>
+{
+    // If you want to use server-side rendering of React components,
+    // add all the necessary JavaScript files here. This includes
+    // your components as well as all of their dependencies.
+    // See http://reactjs.net/ for more information. Example:
+    //config
+    //  .AddScript("~/js/First.jsx")
+    //  .AddScript("~/js/Second.jsx");
+
+    // If you use an external build too (for example, Babel, Webpack,
+    // Browserify or Gulp), you can improve performance by disabling
+    // ReactJS.NET's version of Babel and loading the pre-transpiled
+    // scripts. Example:
+    //config
+    //  .SetLoadBabel(false)
+    //  .AddScriptWithoutTransform("~/js/bundle.server.js");
+});
+
 app.UseStaticFiles();
 
 app.UseRouting();
