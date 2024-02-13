7bd10bbfc36fbc7dc6327654fd8861a3a1937601
Ruben <rmj1523@gmail.com>
2024-02-12 22:04:07 -0500

    save

---
+++ AzureStorageApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
+
+namespace AzureStorageApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Build app with app settings file
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true);
+            IConfiguration config = builder.Build();
+
+            string? keyVaultName = config.GetValue<string>("StorageKey");
+            Console.WriteLine(keyVaultName);
+            var kvUri = "https://" + keyVaultName + ".vault.azure.net";
+
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+            // Get app setting value for storage key
+            Console.WriteLine("Hello, World!");
+
+            Console.ReadLine();
+
+        }
+    }
+}
