32853a9046550defc52fe224396cd9f2ee5cdc1f
Ruben <rmj1523@gmail.com>
2024-02-21 16:17:32 -0500

    save

--- AzureStorageApp/Program.cs
+++ AzureStorageApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
+using Azure.Storage.Blobs;
 using Microsoft.Extensions.Configuration;
 
 namespace AzureStorageApp
@@ -14,16 +15,22 @@ namespace AzureStorageApp
                 .AddJsonFile("appsettings.json", false, true);
             IConfiguration config = builder.Build();
 
+            // Get Key Vault URL and storage key name from app settings
             string keyVaultUrl = config.GetConnectionString("KeyVault") ?? string.Empty;
             string keyVaultStorageName = config.GetValue<string>("StorageKey") ?? string.Empty;
             Console.WriteLine(keyVaultUrl);
 
+            // Get storage key from Key Vault
             var creds = new DefaultAzureCredential(new DefaultAzureCredentialOptions 
             {
                 ManagedIdentityClientId = "7494deba-68fe-48fe-a074-aef13a3446be"
             });
+            
             var client = new SecretClient(new Uri(keyVaultUrl), creds);
             var storageKey = client.GetSecret(keyVaultStorageName);
+
+            // Create a new BlobServiceClient
+            var blobServiceClient = new BlobServiceClient(storageKey.Value.Value);
 
             // Get app setting value for storage key
             Console.WriteLine("Hello, World!");
