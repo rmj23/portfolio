f73add079d91f87bdbe4bf72c20430d6e0182c03
Ruben Morales Jr <rmj1523@gmail.com>
2024-04-03 09:19:39 -0400

    add message sender app

---
+++ MessageSenderApp/MessageSenderApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MessageSenderApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
